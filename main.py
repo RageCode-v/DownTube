91d4f258fad4b91d81e00d062d6eada8f3b6dfec
Rage(618).code() <94807976+RageCode-v@users.noreply.github.com>
2022-01-25 20:11:16 -0300

    Tentando rodar pytube no android

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.core.window import Window
 from pytube import YouTube
+
+Window.softinput_mode = 'below_target'
 
 
 class Adm(ScreenManager):
@@ -8,11 +11,13 @@ class Adm(ScreenManager):
 
 
 class Tel1(Screen):
+    path = '/storage/emulated/0/Ydown'
+
     def get_video(self, url):
         vid = YouTube(str(url))
         self.ids.url.text = ''
         down = vid.streams.get_by_itag(22)
-        down.download('tests')
+        down.download(self.path)
 
 
 class Main(App):
