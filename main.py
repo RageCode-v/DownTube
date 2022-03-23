dfbc8799281cc7d233787623f4104205873b9c88
RageCode-v <victor1gabriel618@gmail.com>
2022-03-22 23:30:32 -0300

    Adicionado uma forma de obter dados do vídeo e exibi-los

--- main.py
+++ main.py
@@ -1,42 +1,60 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.label import Label
+from kivy.utils import platform
+from kivy.metrics import sp
 from pytube import *
-from os.path import join, dirname
+from pytube.exceptions import RegexMatchError
+from os.path import dirname, join
+from os import remove
+from PIL import Image
+import requests
+from io import BytesIO
+
+if platform == 'android':
+    path = join(dirname(str(App.user_data_dir)), 'DownTube')
+else:
+    path = 'E:\\TestsDownTube'
 
 
-class Download(object):
-    def __init__(self, link):
-        self.vid = YouTube(link)
-        self.title = self.vid.title
-        self.st = self.vid.streams.first()
-        self.PATH = join(dirname(str(App.user_data_dir)), 'Download')
-
-    def baixar(self):
-        self.st.download(self.PATH, self.title)
-
-
-class Adm(ScreenManager):
+class ADM(ScreenManager):
     pass
 
 
-class First(Screen):
-    def test(self, url):
-        self.ids.text.text = ''
+class Prince(Screen):
+    def start(self, url):
         try:
-            at = Download(str(url))
-        except Exception as ero:
-            self.ids.demo.text = str(ero)
+            vid = YouTube(str(url))
+        except RegexMatchError:
+            self.ids.direct.text = ''
+        except Exception as erro:
+            self.ids.direct.text = str(erro)
         else:
-            self.ids.demo.text = at.title
-            try:
-                at.baixar()
-            except Exception as erro:
-                self.ids.demo.text = str(erro)
+            response = requests.get(vid.thumbnail_url)
+            img = Image.open(BytesIO(response.content))
+            img.save('./data/tumb.png')
+            self.ids.tumb.source = './data/tumb.png'
+            self.ids.tumb.reload()
+            remove('./data/tumb.png')
+            self.ids.vitle.text = vid.title
+
+
+class Latitle(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.font_size = sp(20)
+
+    def on_size(self, *args):
+        self.text_size = (self.width - sp(10), None)
+
+    def on_texture_size(self, *args):
+        self.size = self.texture_size
+        self.height += sp(20)
 
 
 class Main(App):
     def build(self):
-        return Adm()
+        return ADM()
 
 
 Main().run()
