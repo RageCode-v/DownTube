a58925774a95c3adaa6cf201e113126fec786a62
RageCode-v <victor1gabriel618@gmail.com>
2022-04-14 18:09:20 -0300

    Adicionado uso do KivyMd e novas adaptações

--- app/main.py
+++ app/main.py
@@ -1,161 +1,161 @@
-from kivy.app import App
 from kivy.uix.screenmanager import Screen
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
-from kivy.uix.progressbar import ProgressBar
-from kivy.utils import platform
-from kivy.metrics import sp
-from kivy.network.urlrequest import UrlRequest
+from kivy.uix.floatlayout import FloatLayout
 
-from pytube import *
-from pytube.exceptions import RegexMatchError
+from kivymd.app import MDApp
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.button import MDRectangleFlatIconButton
+from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
 
-from os.path import join
-from os import remove, environ
+from os import environ
 
 from certifi import where
 
 from threading import Thread
 
+from logic import *
+
 environ['SSL_CERT_FILE'] = where()
 pamg = './data/image.png'
-tumb = './data/tumb.png'
+path = down_path()
+colors = {
+    'DeepOrange': {
+        '50': 'FBE9E7',
+        '100': 'FFCCBC',
+        '200': 'FFAB91',
+        '300': 'FF8A65',
+        '400': 'FF7043',
+        '500': 'FF5722',
+        '600': 'F4511E',
+        '700': 'E64A19',
+        '800': 'D84315',
+        '900': 'BF360C',
+        'A100': 'FF9E80',
+        'A200': 'FF6E40',
+        'A400': 'FF3D00',
+        'A700': 'DD2C00'
+    },
+    'Amber': {
+        '50': 'FFF8E1',
+        '100': 'FFECB3',
+        '200': 'FFE082',
+        '300': 'FFD54F',
+        '400': 'FFCA28',
+        '500': 'FFC107',
+        '600': 'FFB300',
+        '700': 'FFA000',
+        '800': 'FF8F00',
+        '900': 'FF6F00',
+        'A100': 'FFE57F',
+        'A200': 'FFD740',
+        'A400': 'FFC400',
+        'A700': 'FFAB00'
+    },
+    'Red': {
+        '50': 'FFEBEE',
+        '100': 'FFCDD2',
+        '200': 'EF9A9A',
+        '300': 'E57373',
+        '400': 'EF5350',
+        '500': 'F44336',
+        '600': 'E53935',
+        '700': 'D32F2F',
+        '800': 'C62828',
+        '900': 'B71C1C',
+        'A100': 'FF8A80',
+        'A200': 'FF5252',
+        'A400': 'FF1744',
+        'A700': 'D50000'
+    },
+    'Light': {
+        'StatusBar': '000000',
+        'AppBar': '212121',
+        'Background': '303030',
+        'CardsDialogs': '424242'
+    },
+    'Dark': {
+        'StatusBar': '000000',
+        'AppBar': '212121',
+        'Background': '303030',
+        'CardsDialogs': '424242'
+    }
+}
 
-if platform == 'android':
-    from android.permissions import request_permissions, Permission
-    from android.storage import primary_external_storage_path
-    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-    path = join(primary_external_storage_path(), 'Download')
-else:
-    path = 'E:\\TestsDownTube'
+
+class TabVid(FloatLayout, MDTabsBase):
+    pass
+
+
+class TabAud(FloatLayout, MDTabsBase):
+    pass
+
+
+class Check(MDRectangleFlatIconButton, MDToggleButton):
+    back1 = 'radiobox-blank'
+    back2 = 'radiobox-marked'
+
+    def __init__(self, **kwargs):
+        super(Check, self).__init__(**kwargs)
+        self.line_color = (0, 0, 0, 0)
+        self.pos_hint = {'center_x': .5, 'center_y': .5}
+        self.icon = self.back1
+        self.font_color_normal = (1, 1, 1, 1)
+
+    def on_state(self, widget, value):
+        if value == 'down':
+            self.icon = self.back2
+        else:
+            self.icon = self.back1
 
 
 class Prince(Screen):
-    obvid = None
-    op = {}
-    probar = ProgressBar(max=100)
-    pop = Popup(title='Downloading', content=probar, size_hint=(.5, .3))
+    obvid: any
+    op: str
 
     def reset(self):
         self.ids.direct.text = ''
         self.ids.direct.reset_undo()
         self.ids.tumb.source = pamg
         self.ids.vitle.text = 'Video title'
-        self.obvid = None
-        self.op = {}
-        self.probar.value = 0
-        self.pop.title = 'Downloading'
+        del self.obvid
+        del self.op
 
-    def start(self, url):
+    def get(self, url):
         try:
-            self.obvid = YouTube(str(url), on_progress_callback=self.update_p, on_complete_callback=self.status)
-        except RegexMatchError:
+            self.obvid = Order(url)
+        except LinkNotFound:
             self.ids.direct.text = ''
             self.ids.tumb.source = pamg
             self.ids.vitle.text = 'Vídeo não encontrado'
         except Exception as erro:
-            self.ids.direct.text = str(erro)
+            print(erro)
         else:
             try:
-                UrlRequest(self.obvid.thumbnail_url, file_path=tumb).wait()
+                self.ids.tumb.source = str(self.obvid.tumb)
             except Exception as erro:
-                self.ids.direct.text = str(erro)
+                print(erro)
             else:
-                self.ids.tumb.source = tumb
-                remove(tumb)
                 self.ids.vitle.text = self.obvid.title
 
-    def update_p(self, stream, chunk, bytes_remaining):
-        def porcent(te, tot):
-            perc = (float(te) / float(tot)) * float(100)
-            return perc
-
-        size = stream.filesize
-        p = porcent(bytes_remaining, size)
-        self.probar.value = abs(p-100)
-
-    def select(self, opt):
+    def select(self, opt: str):
         self.op = opt
-        del opt
 
     def down_this(self):
-        if self.op == {}:
+        try:
+            Thread(target=self.obvid.down_this, args=(path, self.op)).start()
+        except AttributeError:
             pass
+        except Exception as erro:
+            print(erro)
         else:
-            try:
-                st = self.obvid.streams.get_by_itag(self.op['itag'])
-            except AttributeError:
-                pass
-            except KeyError:
-                pass
-            except Exception as erro:
-                self.ids.direct.text = str(erro)
-            else:
-                try:
-                    Thread(target=st.download, args=(path, ((self.obvid.title.replace('/', ' ').replace('\\', ' ')) +
-                                                            self.op['ext']), )).start()
-                except Exception as erro:
-                    self.ids.direct.text = str(erro)
-                else:
-                    self.status(isopen=True)
-
-    def status(self, *args, **kwargs):
-        try:
-            isopen = kwargs['isopen']
-        except KeyError:
-            isopen = False
-        if isopen:
-            self.pop.open()
-        else:
-            self.pop.title = 'COMPLETO'
-            self.pop.dismiss()
+            self.reset()
 
 
-class Latitle(Label):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.font_size = sp(20)
-
-    def on_size(self, *args):
-        self.text_size = (self.width - sp(10), None)
-
-    def on_texture_size(self, *args):
-        self.size = self.texture_size
-        self.height += sp(20)
-
-
-class Main(App):
-    def on_pause(self):
-        try:
-            with open(tumb, 'r'):
-                pass
-        except FileNotFoundError:
-            pass
-        else:
-            with open('./data/urlsave.txt', 'w') as f:
-                f.write(str(Prince.ids.direct.text))
-        return True
-
-    def on_resume(self):
-        try:
-            with open(tumb, 'r'):
-                pass
-        except FileNotFoundError:
-            Prince().reset()
-        else:
-            try:
-                with open('./data/urlsave.txt', 'r') as f:
-                    vtx = f.read()
-            except FileNotFoundError:
-                remove(tumb)
-                Prince().reset()
-            else:
-                remove('./data/urlsave.txt')
-                Prince.ids.direct.text = vtx
-                Prince.ids.tumb.source = tumb
-                del vtx
-
+class Main(MDApp):
     def build(self):
+        self.theme_cls.colors = colors
+        self.theme_cls.primary_palette = 'Amber'
+        self.theme_cls.accent_palette = 'DeepOrange'
+        self.theme_cls.theme_style = 'Dark'
         return Prince()
 
 
