6bacd3564d990d35073406b883e4c9c1f6d586fc
RageCode-v <victor1gabriel618@gmail.com>
2022-04-20 15:47:07 -0300

    Colors movido para arquivo JSON em data

--- app/logic/__init__.py
+++ app/logic/__init__.py
@@ -2,3 +2,11 @@ from logic.notify import notbar
 from logic.wherepath import down_path
 from logic.downloader import Order
 from logic.exceptions import *
+
+
+def load_colors():
+    from json import loads
+
+    with open('data/colors.json', 'r') as f:
+        res = loads(f.read())
+    return res
--- app/main.py
+++ app/main.py
@@ -6,11 +6,10 @@ from kivymd.app import MDApp
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.button import MDRectangleFlatIconButton
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivymd.toast import toast
 
 from os import environ
-
 from certifi import where
-
 from threading import Thread
 
 from logic import *
@@ -21,68 +20,7 @@ if platform == 'android':
 environ['SSL_CERT_FILE'] = where()
 pamg = './data/image.png'
 path = down_path()
-colors = {
-    'DeepOrange': {
-        '50': 'FBE9E7',
-        '100': 'FFCCBC',
-        '200': 'FFAB91',
-        '300': 'FF8A65',
-        '400': 'FF7043',
-        '500': 'FF5722',
-        '600': 'F4511E',
-        '700': 'E64A19',
-        '800': 'D84315',
-        '900': 'BF360C',
-        'A100': 'FF9E80',
-        'A200': 'FF6E40',
-        'A400': 'FF3D00',
-        'A700': 'DD2C00'
-    },
-    'Amber': {
-        '50': 'FFF8E1',
-        '100': 'FFECB3',
-        '200': 'FFE082',
-        '300': 'FFD54F',
-        '400': 'FFCA28',
-        '500': 'FFC107',
-        '600': 'FFB300',
-        '700': 'FFA000',
-        '800': 'FF8F00',
-        '900': 'FF6F00',
-        'A100': 'FFE57F',
-        'A200': 'FFD740',
-        'A400': 'FFC400',
-        'A700': 'FFAB00'
-    },
-    'Red': {
-        '50': 'FFEBEE',
-        '100': 'FFCDD2',
-        '200': 'EF9A9A',
-        '300': 'E57373',
-        '400': 'EF5350',
-        '500': 'F44336',
-        '600': 'E53935',
-        '700': 'D32F2F',
-        '800': 'C62828',
-        '900': 'B71C1C',
-        'A100': 'FF8A80',
-        'A200': 'FF5252',
-        'A400': 'FF1744',
-        'A700': 'D50000'
-    },
-    'Light': {
-        'StatusBar': '000000',
-        'AppBar': '212121',
-        'Background': '303030',
-        'CardsDialogs': '424242'
-    },
-    'Dark': {
-        'StatusBar': '000000',
-        'AppBar': '212121',
-        'Background': '303030',
-        'CardsDialogs': '424242'
-    }
-}
+colors = load_colors()
 
 
 class TabVid(FloatLayout, MDTabsBase):
@@ -151,6 +89,7 @@ class Prince(Screen):
         except Exception as erro:
             print(erro)
         else:
+            toast('Download iniciado')
             self.reset()
 
 
