437637074aa11fd4ce75598730135796c1866c92
RageCode-v <victor1gabriel618@gmail.com>
2022-05-05 18:06:50 -0300

    usando Java para as notificações no android - Possível solução de #3

---
+++ JavaFile/Notify.java
@@ -0,0 +1,80 @@
+package com.ragecodev.downtube;
+
+import android.content.Context;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.Notification;
+import android.os.Build;
+import com.ragecodev.downtube.R;
+import java.lang.Math;
+
+public class Notify{
+    public void iniciar(Context context, CharSequence name, int percent) {
+        this.createChannelProgress(context);
+        this.createChannelFinal(context);
+        this.sendNotification(context, name, percent);
+    }
+
+    private void createChannelProgress(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            CharSequence name = "Downloads ativo";
+            String desc = "Exibir progresso de download";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("DownActives", name, importance);
+            channel.setDescription(desc);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void createChannelFinal(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            CharSequence name = "Downloads completos";
+            String desc = "Exibir o download após completo";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CompleteDowns", name, importance);
+            channel.setDescription(desc);
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void sendNotification(Context context, CharSequence name, int percent) {
+        String notification_id = name.toString().substring(0, 3);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "DownActives");
+        builder.setContentTitle(name)
+                .setContentText(percent + "%")
+                .setSmallIcon(R.drawable.download)
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+        if (percent < 100) {
+            builder.setProgress(100, percent, false);
+        }
+        else {
+            builder.setProgress(0, 0, false);
+            this.finalNotify(context, name);
+        }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(notification_id, builder.build());
+    }
+
+    private void finalNotify(Context context, CharSequence name) {
+        int notification_id = (int)(Math.random()*(8000-1+1)+1);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "CompleteDowns");
+        builder.setContentTitle(name)
+                .setContentText("Download concluído")
+                .setSmallIcon(R.drawable.check-outline)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(notification_id, builder.build());
+    }
+}
--- app/logic/notify.py
+++ app/logic/notify.py
@@ -1,57 +1,23 @@
 from kivy.utils import platform
 
 if platform == 'android':
-    from jnius import autoclass, cast
-    from logic.exceptions import NotificationChannelFailed
+    from jnius import autoclass
 
-    Apptivity = autoclass('org.kivy.android.PythonActivity').mActivity
-    context = Apptivity.getApplicationContext()
-    AndroidString = autoclass('java.lang.String')
-    NotiManager = autoclass('android.app.NotificationManager')
-    NotiChannel = autoclass('android.app.NotificationChannel')
-    NotiCompat = autoclass('androidx.core.app.NotificationCompat')
-    NotiComBuild = autoclass('androidx.core.app.NotificationCompat$Builder')
-    NotiManCompat = autoclass('androidx.core.app.NotificationManagerCompat')
-    func_from = getattr(NotiManCompat, 'from')
-    channel_id = AndroidString('DownsOfTube')
+    mActivity = autoclass('org.kivy.android.PythonActivity').mActivity
+
+    context = mActivity.getApplicationContext()
+    javacl = autoclass('com.ragecodev.downtube.Notify')
+    notifyJ = javacl()
 
 
-    def channel_creator():
-        name = cast('java.lang.CharSequence', AndroidString('DownTube downloads'))
-        desc = AndroidString('Progresso de downloads do DownTube')
-        importance = NotiManager.IMPORTANCE_DEFAULT
+    def notbar(name, percent):
+        percent = int(percent)
 
-        channel = NotiChannel(channel_id, name, importance)
-        channel.setDescription(desc)
-
-        notimana = context.getSystemService(NotiManager)
-        try:
-            notimana.createNotificationChannel(channel)
-        except Exception as erro:
-            print(erro)
-            raise NotificationChannelFailed
-
-
-    def notbar(name, percent, first):
-        builder = NotiComBuild(context, channel_id)
-        if first:
-            builder.setContentTitle(cast('java.lang.CharSequence', AndroidString(str(name))))
-            builder.setContentText(cast('java.lang.CharSequence', AndroidString('Downloading')))
-            builder.setSmallIcon(context.getApplicationInfo().icon)
-            builder.setPriority(NotiCompat.PRIORITY_LOW)
-            builder.setVisibility(NotiCompat.VISIBILITY_PUBLIC)
-            builder.setProgress(cast('java.lang.Integer', int(100)), cast('java.lang.Integer', int(0)), cast(
-                'java.lang.Boolean', bool(False)))
-        else:
-            builder.setProgress(cast('java.lang.Integer', int(100)), cast('java.lang.Integer', int(percent)), cast(
-                'java.lang.Boolean', bool(False)))
-
-        copatmana = NotiManCompat.func_from(context)
-        copatmana.notify(str(name[:3]), builder.build())
+        notifyJ.iniciar(context, name, percent)
 
 
 else:
-    def notbar(name, percent, nah):
+    def notbar(name, percent):
         if percent < 100:
             print(f'\033[36m{name}\n    \033[32m{percent:.0f}% completo')
         else:
